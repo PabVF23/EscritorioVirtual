943e14619660ca3b185eeebb8ddaea51aca4e3e7
PabVF23 <UO282655@uniovi.es>
2023-11-05 16:55:06 +0100

    Ejercicio 5

---
+++ xml/xml2perfil.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+
+def verXML(archivoXML):
+    try:
+        arbol = ET.parse(archivoXML)
+    except IOError:
+        print('No se encuentra el archivo', archivoXML)
+        exit()
+    except ET.ParseError:
+        print("Error procesando el archivo XML = ", archivoXML)
+        exit()
+    
+    raiz = arbol.getroot()
+    
+    print("\nElemento raiz = ", raiz.tag)
+    if raiz.text != None:
+        print("Contenido = ", raiz.text.strip('\n'))
+    else:
+        print("Contenido = ", raiz.text)
+    
+    print("Atributos = ", raiz.attrib)
+    
+    for hijo in raiz.findall(".//"):
+        print("\nElemento = ", hijo.tag)
+        if hijo.text != None:
+            print("Contenido = ", hijo.text.strip('\n'))
+        else:
+            print("Contenido = ", hijo.text)
+        
+        print("Atributos = ", hijo.attrib)
+        
+def escribirKML(archivoXML):
+    try:
+        arbol = ET.parse(archivoXML)
+    except IOError:
+        print('No se encuentra el archivo', archivoXML)
+        exit()
+    except ET.ParseError:
+        print("Error procesando el archivo XML = ", archivoXML)
+        exit()
+        
+    raiz = arbol.getroot()
+    
+    index = 0
+    
+    ns = "http://www.uniovi.es"
+    rutaXPath = ".//{" + ns + "}ruta"
+    
+    for ruta in raiz.findall(rutaXPath):
+        index += 1
+        filename = "perfil" + str(index) + ".svg"
+        f = open(filename, "w")
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write('<svg version="2.0" xmlns="http://www.w3.org/2000/svg">\n')
+        
+        
+        f.write('\t<polyline points = "\n')
+        xPos = 10
+
+        coordsXPath = ".//{" + ns + "}coordenadas"
+
+        for coord in ruta.findall(coordsXPath):
+            atributos = coord.attrib
+            altitud = atributos['altitud']
+            f.write("\t\t{0},{1}\n".format(xPos, 160 - float(altitud)))
+
+            xPos += 20
+
+        f.write("\t\t{0},160\n".format(xPos))
+
+        altitudInicial = ruta.findall(coordsXPath + "[1]")[0].attrib['altitud']
+
+        f.write("\t\t10,{0}\n".format(160 - float(altitudInicial)))
+        
+        f.write('\t\t" style="fill:white;stroke:red;stroke-width:4" />\n')
+        
+        nombre = ruta.find(".//{" + ns +"}lugar")
+
+        f.write('\t\t<text x="10" y="165" style="writing-mode: tb; glyph-orientation-vertical: 0;">\n')
+        f.write("\t\t\t" + nombre.text + "\n")
+        f.write("\t\t</text>\n")
+
+        xPos = 30
+        for hito in ruta.findall(".//{" + ns + "}hito"):
+            f.write('\t\t<text x="{0}" y="165" style="writing-mode: tb; glyph-orientation-vertical: 0;">\n'.format(xPos))
+            f.write("\t\t\t" + hito.attrib['nombre'] + "\n")
+            f.write("\t\t</text>\n")
+
+            xPos += 20
+
+        f.write('\t\t<text x="{0}" y="165" style="writing-mode: tb; glyph-orientation-vertical: 0;">\n'.format(xPos))
+        f.write("\t\t\t" + nombre.text + "\n")
+        f.write("\t\t</text>\n")
+
+        f.write('</svg>\n')
+        
+def main():
+    miArchivoXML = input('Introduzca un archivo XML = ')
+    verXML(miArchivoXML)
+    escribirKML(miArchivoXML)
+    
+if __name__ == "__main__":
+    main()
