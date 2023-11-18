a6c87da5a7a0fa758aeeebf0401484167a55b34c
PabVF23 <UO282655@uniovi.es>
2023-11-18 13:15:31 +0100

    Juego implementado

--- js/memoria.js
+++ js/memoria.js
@@ -22,4 +22,95 @@ class Memoria {
         {"element": "SVG", "source": "https://upload.wikimedia.org/wikipedia/commons/4/4f/SVG_Logo.svg"},
         {"element": "W3C", "source": "https://upload.wikimedia.org/wikipedia/commons/5/5e/W3C_icon.svg"}
     ]
+
+    constructor() {
+        this.hasFlippedCard = false;
+        this.lockBoard = false;
+        this.firstCard = null;
+        this.secondCard = null;
+
+        
+        this.shuffleElements();
+        this.createElements();
+        this.addEventListeners();
+    }
+
+    shuffleElements() {
+        for (var i = this.elements.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = this.elements[i];
+            this.elements[i] = this.elements[j];
+            this.elements[j] = temp;
+        }
+    }
+
+    unflipCards() {
+        this.lockBoard = true;
+        this.firstCard.removeAttribute("data-state");
+        this.secondCard.removeAttribute("data-state");
+
+        setTimeout(() => {
+            this.resetBoard();
+        }, 5)
+    }
+
+    resetBoard() {
+        this.hasFlippedCard = false;
+        this.lockBoard = false;
+        this.firstCard = null;
+        this.secondCard = null;
+    }
+
+    checkForMatch() {
+        this.firstCard.getAttribute("data-element") === this.secondCard.getAttribute("data-element") ? this.disableCards() : this.unflipCards();
+    }
+
+    disableCards() {
+        this.firstCard.setAttribute("data-state", "revealed")
+        this.secondCard.setAttribute("data-state", "revealed")
+
+        this.resetBoard();
+    }
+
+    createElements() {
+        this.elements.forEach((element) => {
+            var aux = "<article data-element=\"" + element.element + "\">\n";
+            aux +="\t<h3>Tarjeta de memoria</h3>\n";
+            aux += "\t<img src=\"" + element.source + "\" alt = \"" + element.element + "\"/>\n";
+            aux += "</article>"
+
+            document.write(aux)
+        });
+    }
+
+    addEventListeners() {
+        var cards = document.querySelectorAll("article");
+        cards.forEach((card) => {
+            var flipCard = this.flipCard.bind(card, this)
+            card.onclick = function(event) {
+                flipCard();
+            };
+        });
+    }
+
+    flipCard(game) {
+        if (this.getAttribute("data-state") === "revealed" || game.lockBoard || this === game.firstCard) {
+            return;
+        }
+        
+        this.setAttribute("data-state", "flip");
+
+        setTimeout(() => {
+            if (game.hasFlippedCard) {
+                game.secondCard = this;
+                game.checkForMatch();
+            } else {
+                game.hasFlippedCard = true;
+                game.firstCard = this;
+            }
+        }, 500)
+        
+    }
 }
+
+var memoria = new Memoria();
--- js/pais.js
+++ js/pais.js
@@ -2,7 +2,7 @@ class Pais {
     constructor(nombre, capital, poblacion) {
         this.nombre = nombre;
         this.capital = capital;
-        this.poblacion = poblacion
+        this.poblacion = poblacion;
     }
 
     setTipoGobierno(tipoGobierno) {
