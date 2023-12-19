99e45fc3c2e0dbc6956d0a5e4c86e7b9ccff84ca
PabVF23 <UO282655@uniovi.es>
2023-12-19 12:26:54 +0100

    no sql

--- php/records.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `registro` (
- `nombre` varchar(100) NOT NULL,
- `apellidos` varchar(100) NOT NULL,
- `nivel` varchar(40) NOT NULL,
- `tiempo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
